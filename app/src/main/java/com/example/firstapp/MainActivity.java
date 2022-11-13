1ee89676a8b64f4a3d3f3ba8ea74cd0991be82b3
Masim1001 <asimbhatti1001@gmail.com>
2022-11-14 04:01:19 +0500

    Create variables for each view control

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_answer0, btn_answer1, btn_answer2, btn_answer3;
+    TextView tv_score, tv_questions, tv_timer, tv_bottomMsg;
+    ProgressBar prog_timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_answer0 = findViewById(R.id.btn_answer0);
+        btn_answer1 = findViewById(R.id.btn_answer1);
+        btn_answer2 = findViewById(R.id.btn_answer2);
+        btn_answer3 = findViewById(R.id.btn_answer3);
+
+        tv_score = findViewById(R.id.tv_score);
+        tv_questions = findViewById(R.id.tv_questions);
+        tv_timer = findViewById(R.id.tv_timer);
+        tv_bottomMsg = findViewById(R.id.tv_bottomMsg);
+
+        prog_timer = findViewById(R.id.prog_timer);
+
+        tv_timer.setText("0Sec");
+        tv_questions.setText("");
+        tv_bottomMsg.setText("Press Go");
+        tv_score.setText("0 points");
     }
 }
